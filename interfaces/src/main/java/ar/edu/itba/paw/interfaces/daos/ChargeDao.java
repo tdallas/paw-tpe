f1ef4fab82957a5c44b08ab3c21005869ddbb9a4
tdallas <tdallas@itba.edu.ar>
2019-10-04 16:58:39 -0300

    added chargesDTO list on reservation id

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/daos/ChargeDao.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/daos/ChargeDao.java
@@ -1,7 +1,12 @@
 package ar.edu.itba.paw.interfaces.daos;
 
 import ar.edu.itba.paw.models.charge.Charge;
+import ar.edu.itba.paw.models.dtos.ChargeDTO;
+
+import java.util.List;
 
 public interface ChargeDao extends SimpleDao<Charge> {
     boolean addCharge(Charge product);
+
+    List<ChargeDTO> findChargeByReservationHash(long reservationId);
 }
--- persistence/src/main/java/ar/edu/itba/paw/persistence/ChargeRepository.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/ChargeRepository.java
@@ -2,13 +2,18 @@ package ar.edu.itba.paw.persistence;
 
 import ar.edu.itba.paw.interfaces.daos.ChargeDao;
 import ar.edu.itba.paw.models.charge.Charge;
+import ar.edu.itba.paw.models.dtos.ChargeDTO;
+import ar.edu.itba.paw.models.product.Product;
+import ar.edu.itba.paw.models.reservation.Reservation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 @Repository
 public class ChargeRepository extends SimpleRepository<Charge> implements ChargeDao {
@@ -20,9 +25,9 @@ public class ChargeRepository extends SimpleRepository<Charge> implements Charge
         super(new NamedParameterJdbcTemplate(dataSource));
         jdbcTemplateWithNamedParameter.getJdbcTemplate()
                 .execute("CREATE TABLE IF NOT EXISTS " + getTableName() + " (" +
-                "id SERIAL PRIMARY KEY, " +
-                "product_id INTEGER REFERENCES product (id), " +
-                "reservation_id INTEGER REFERENCES reservation (id))");
+                        "id SERIAL PRIMARY KEY, " +
+                        "product_id INTEGER REFERENCES product (id), " +
+                        "reservation_id INTEGER REFERENCES reservation (id))");
     }
 
     @Override
@@ -48,5 +53,19 @@ public class ChargeRepository extends SimpleRepository<Charge> implements Charge
             return false;
         }
         return true;
+    }
+
+    @Override
+    public List<ChargeDTO> findChargeByReservationHash(long reservationId) {
+        MapSqlParameterSource parameters = new MapSqlParameterSource();
+        parameters.addValue("reservationId", reservationId);
+        return jdbcTemplateWithNamedParameter
+                .query("SELECT * FROM " + Charge.TABLE_NAME + " NATURAL JOIN " +
+                        Product.TABLE_NAME + " NATURAL JOIN " + Reservation.TABLE_NAME +
+                        " r WHERE r.id = :reservationId", parameters, getRowMapperOfChargeDTO());
+    }
+
+    private RowMapper<ChargeDTO> getRowMapperOfChargeDTO() {
+        return ((resultSet, i) -> new ChargeDTO(resultSet));
     }
 }
