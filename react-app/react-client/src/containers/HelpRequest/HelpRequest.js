e67da16e7bcd0b2d9fec538509b5f58720c788b6
fermartin17 <femartin@itba.edu.ar>
2021-06-30 16:48:34 -0300

    help request error handling

--- react-app/react-client/src/containers/HelpRequest/HelpRequest.js
+++ react-app/react-client/src/containers/HelpRequest/HelpRequest.js
@@ -1,12 +1,14 @@
-import React, {useState} from "react";
-import {Container, Row, Col} from "react-bootstrap";
-import {makeStyles} from "@material-ui/core/styles";
-import {withRouter} from "react-router";
+import React, { useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { makeStyles } from "@material-ui/core/styles";
+import { withRouter } from "react-router";
 import Button from "../../components/Button/Button";
 import Table from '../../components/Table/Table'
-import {useTranslation} from "react-i18next";
-import {getAllHelpRequests} from "../../api/helpApi";
-import {helpListColumns} from "../../utils/columnsUtil";
+import { useTranslation } from "react-i18next";
+import { getAllHelpRequests } from "../../api/helpApi";
+import { helpListColumns } from "../../utils/columnsUtil";
+
+import Modal from 'react-bootstrap/Modal'
 
 
 const useStyles = makeStyles((theme) => ({
@@ -28,18 +30,22 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-const HelpRequest = ({history}) => {
+const HelpRequest = ({ history }) => {
     const classes = useStyles();
-    const {t} = useTranslation();
-    const [tableInfo, setTableInfo] = useState({helpList: [], totalCount: 0});
-    const {helpList, totalCount} = tableInfo;
+    const { t } = useTranslation();
+    const [tableInfo, setTableInfo] = useState({ helpList: [], totalCount: 0 });
+    const { helpList, totalCount } = tableInfo;
+
+    const handleCloseMessage = () => setShowMessage(false);
+    const [showMessage, setShowMessage] = useState(false);
 
     const getAllHelpRequestsUnsolved = (page, limit) => {
-        getAllHelpRequests({page, limit})
+        getAllHelpRequests({ page, limit })
             .then((response) => {
-                setTableInfo({helpList: response.data, totalCount: +response.headers["x-total-count"]})
+                setTableInfo({ helpList: response.data, totalCount: +response.headers["x-total-count"] })
             }
-        );
+            )
+            .catch((response) => setShowMessage(true));
     };
 
     const onSubmitHelpRequest = () => {
@@ -63,15 +69,31 @@ const HelpRequest = ({history}) => {
                         />
                     </Col>
                     <Col xs={12} md={2}>
-                        <Col xs={12} md={6} style={{textAlign: 'left'}}>
+                        <Col xs={12} md={6} style={{ textAlign: 'left' }}>
                             <Button ButtonType="Save" size="large" onClick={onSubmitHelpRequest}
-                                    ButtonText={t("refresh")}/>
+                                ButtonText={t("refresh")} />
                         </Col>
-                        <Col xs={12} md={6} style={{textAlign: 'left'}}>
-                            <Button ButtonType="Back" size="large" onClick={back} ButtonText={t("back")}/>
+                        <Col xs={12} md={6} style={{ textAlign: 'left' }}>
+                            <Button ButtonType="Back" size="large" onClick={back} ButtonText={t("back")} />
                         </Col>
                     </Col>
                 </Row>
+
+                <Modal centered show={showMessage} onHide={handleCloseMessage}>
+                    <Modal.Body>
+                        <Row>
+                            <Col xs={1} sm={1}></Col>
+                            <Col xs={10} sm={10}>
+                                <h4>{t("something_happened")}</h4>
+                            </Col>
+                            <Col xs={1} sm={1}></Col>
+                        </Row>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button ButtonType="Save" size="large" onClick={handleCloseMessage}
+                            ButtonText={t("accept")} />
+                    </Modal.Footer>
+                </Modal>
             </Container>
         </div>
     );
