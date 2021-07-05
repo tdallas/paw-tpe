dbe51b1f639e55f7d3b35cd8bba2d487c0d66e7e
fermartin17 <femartin@itba.edu.ar>
2021-07-05 20:55:12 -0300

    delete comment

--- react-app/react-client/src/api/helpApi.js
+++ react-app/react-client/src/api/helpApi.js
@@ -7,6 +7,5 @@ const aHelpRequestUrl = id => baseRoomURL + `/${id}`;
 
 export const getAllHelpRequests = async (params) => get(getAllHelpRequestsUrl, params);
 export const getAHelpRequest = async (id) => get(aHelpRequestUrl(id));
-// export const updateHelpStep = async (id, params, body) => put(aHelpRequestUrl(id), params, body);
 export const updateHelpStep = async (id,params) => put(aHelpRequestUrl(id), params, {});
 
